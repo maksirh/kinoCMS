0e2c5a3483fcbc84c8cf12a7653c36c505d1ceed
Yorzh Maksym <iorhmaks@gmail.com>
2026-01-20 02:25:01 +0200

    add context for cinemas list, part cinema page and some corrects

--- src/cms/urls.py
+++ src/cms/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import cinema_page
+from .views import cinema_page, cinemas_list
 
 app_name = 'cinema'
 
 urlpatterns = [
-    path('cinemas', cinema_page, name='cinema_page'),
+
+    path('cinemas', cinemas_list, name='cinemas_list'),
+    path('cinema/<int:cinema_id>/', cinema_page, name="cinema_page")
 ]
--- src/cms/views.py
+++ src/cms/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from src.cms.models import Cinema
 
-def cinema_page(request):
-    return render(request, 'cms/cinema.html')
+
+def cinemas_list(request):
+    
+    cinemas = Cinema.objects.all()
+
+    context = {
+        "cinemas": cinemas
+    }
+    return render(request, 'cms/cinemas.html', context)
+
+
+def cinema_page(request, cinema_id):
+
+    cinema = get_object_or_404(Cinema, pk=cinema_id)
+
+    context = {
+        "cinema": cinema,
+    }
+
+    return render(request, 'cms/cinema.html', context)
