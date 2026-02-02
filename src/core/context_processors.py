ef948ad31a56467018cec7652ee10dbccc6db6e9
Yorzh Maksym <iorhmaks@gmail.com>
2026-02-03 01:33:52 +0200

    some fix

--- src/core/context_processors.py
+++ src/core/context_processors.py
@@ -3,9 +3,16 @@ from ..main.models import MainPage, Page
 
 def phone_numbers(request):
     main_page = MainPage.objects.first()
+
+    if main_page:
+        return {
+            "phone1": main_page.phone_number1,
+            "phone2": main_page.phone_number2,
+        }
+
     return {
-        "phone1": main_page.phone_number1,
-        "phone2": main_page.phone_number2,
+        "phone1": "",
+        "phone2": "",
     }
 
 def pages_inf(request):
