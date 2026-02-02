dd73258353ef619ef50ad2e6132d4f006e51f0e2
Yorzh Maksym <iorhmaks@gmail.com>
2026-02-03 00:56:39 +0200

    fix asgi

--- config/asgi.py
+++ config/asgi.py
@@ -1,13 +1,16 @@
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import src.main.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kinoCMS.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             src.main.routing.websocket_urlpatterns
